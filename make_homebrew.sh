b619d967c1b4a4ac4677bab54cc18878117ce603
Matthew Ruttley <matt@frame.io>
2019-03-27 16:53:13 -0400

    Homebrew only recognizes a tarball if all contained files are within a folder

--- make_homebrew.sh
+++ make_homebrew.sh
@@ -3,6 +3,12 @@
 # Usage:
 # ./make.sh 1.0.0
 
-rm -f dima-*.tar.gz
-tar zcf dima-$1.tar.gz dima.py sample_creds README.md deploy.sh setup.py
-shasum -a 256 dima-$1.tar.gz 
+rm -f v*.tar.gz
+
+# homebrew requires the tarball to have an interal folder
+mkdir dima-$1
+cp dima.py {sample_creds,README.md,deploy.sh,setup.py,LICENSE} dima-$1/
+tar zcf v$1.tar.gz dima-$1
+rm -rf dima-$1
+
+shasum -a 256 v$1.tar.gz 
